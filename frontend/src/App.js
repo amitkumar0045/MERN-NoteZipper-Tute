9947f1d61a8fbb1271c55c9fc7b5fe76436775f3
amit.kumar@gupshup.io <amit.kumar@gupshup.io>
2021-09-24 10:05:31 +0530

    Landing_page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+import LandingPage from './screen_pages/LandingPage/LandingPage';
+
+
 
 function App() {
+
   return (
     <div className="App">
-      <h1> starting MERN developing</h1>
+      <Header />
+      <main
+      // style={{ minHeight: "93vh" }} // moved to "LandingPage"
+      >
+        <LandingPage />
+      </main>
+      <Footer />
     </div>
   );
 }
 
 export default App;
+{/* <p> starting MERN developing </p>
+      <span>https://www.youtube.com/playlist?list=PLKhlp2qtUcSYC7EffnHzD-Ws2xG-j3aYo</span> */}
+  // const App = () => <h1 className="App"> Hello World </h1>
---
+++ frontend/src/components/header/Header.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import {
+    Container,
+    Form,
+    FormControl,
+    Nav,
+    Navbar,
+    NavDropdown,
+    Button,
+} from "react-bootstrap";
+
+const Header = () => {
+    return (
+        <div>
+
+            <Navbar bg="primary" expand="lg" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#">Note Zipper</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className='m-auto'>
+                            <Form inline>
+                                <FormControl
+                                    type="search"
+                                    placeholder="Search"
+                                    className="mr-2"
+                                    aria-label="Search"
+                                />
+                                {/* <Button variant="outline-success">Search</Button> */}
+                            </Form>
+                        </Nav>
+
+
+                        <Nav
+                        // className="mr-auto"
+                        // style={{ maxHeight: '100px' }}
+                        // navbarScroll
+                        >
+                            <Nav.Link href="#home">My Notes</Nav.Link>
+                            <NavDropdown title="AAraya" id="basic-navbar-dropdown">
+                                <NavDropdown.Item href="#action3">My Profile</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action5">Logout</NavDropdown.Item>
+                            </NavDropdown>
+                            {/* <Nav.Link href="#" disabled>
+                            Link
+                        </Nav.Link> */}
+                        </Nav>
+
+
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+        </div>
+    )
+}
+
+export default Header
