2a8f641124679b7412122a55dcc1a5d955348a71
amit.kumar@gupshup.io <amit.kumar@gupshup.io>
2021-09-26 14:33:29 +0530

    .... adding new user to db

---
+++ backend/models/userModel.js
@@ -0,0 +1,48 @@
+// Model defines what type of data is user is going to have ...
+
+const mongoose = require('mongoose'); // to create our "userMOdel"
+const bcrypt = require('bcryptjs'); //
+
+const userSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        isAdmin: {
+            //just in case if we need
+            type: Boolean,
+            required: true,
+            default: true,
+        },
+        pic: {
+            type: String,
+            required: true,
+            default: 'https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg'
+        },
+    },
+    {
+        timestamp: true,
+    }
+);
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
--- backend/server.js
+++ backend/server.js
@@ -2,29 +2,38 @@ const express = require('express')
 const notes = require('./data/notes') //dummy  data
 const dotenv = require("dotenv");//contains secrt info like api-keys
 const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes')
+const { notFound, errorHandler } = require('./middlewares/errorMiddleWare')
 
 const app = express();
 dotenv.config();
 connectDB();
+app.use(express.json())
+// app.use(notFound);
+// app.use(errorHandler);
 
-app.get("/", (req, res) => {
-    res.send("API is RUNNING....")
-})
+//\\----
+// app.get("/", (req, res) => {
+//     res.send("API is RUNNING....")
+// })
 
-app.get('/api/notes', (req, res) => {
-    res.json(notes)
-})
+// app.get('/api/notes', (req, res) => {
+//     res.json(notes)
+// })
 
-app.get('/api/notes/:id', (req, res) => {
-    const note = notes.find((n) => n._id === req.params.id);
-    res.send(note);
-})
+// app.get('/api/notes/:id', (req, res) => {
+//     const note = notes.find((n) => n._id === req.params.id);
+//     res.send(note);
+// })
+//\\----
+
+app.use('/api/users', userRoutes)
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(
     PORT,
     console.log(
-        `Server running on port ${PORT}`
+        `Server Started on port ${PORT}`
     )
 );
